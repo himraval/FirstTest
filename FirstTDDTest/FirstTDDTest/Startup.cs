517caf7c7bb14ff62cd49e01f6948059bb442e6f
HIMANI RAVAL <himraval@gmail.com>
2016-03-11 15:08:32 +0530

    Initial project template

---
+++ FirstTDDTest/FirstTDDTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FirstTDDTest.Startup))]
+namespace FirstTDDTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
